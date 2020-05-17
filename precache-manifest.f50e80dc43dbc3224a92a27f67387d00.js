fe66f10c8a20d36ad0e82b1f6362097d846d6d78
sudhir <sudhir@workplacefabric.com>
2020-05-17 11:24:56 +0530

    Updates

---
+++ precache-manifest.f50e80dc43dbc3224a92a27f67387d00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bb637843dac5d879ad5",
+    "url": "/chat/static/js/runtime~main.54cd968c.js"
+  },
+  {
+    "revision": "8e5ea00b28890ca81dda",
+    "url": "/chat/static/js/main.8d9b86b6.chunk.js"
+  },
+  {
+    "revision": "c1f4041ad8a6ee6e3261",
+    "url": "/chat/static/js/2.4baa8df5.chunk.js"
+  },
+  {
+    "revision": "8e5ea00b28890ca81dda",
+    "url": "/chat/static/css/main.7981734a.chunk.css"
+  },
+  {
+    "revision": "74ab3d05b19a57078066076358754f0f",
+    "url": "/chat/index.html"
+  }
+];
